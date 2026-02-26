ed9d11e340de2e666f940c756766e090e7d83f81
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-02-27 00:50:06 +0530

    first commit

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://your-frontend-domain.com",
+      "http://localhost:5173"
+    //   "https://your-frontend-domain.com",
     ],
     credentials: true,
   })
