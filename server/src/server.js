da46a0b0397df29154cf6a2224862ed3ab9b09b1
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-02-27 00:48:03 +0530

    first commit

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
 const app = require("./app");
-// Routes
-app.use("/api/auth", require("./modules/auth/auth.routes"));
-app.use("/api/books", require("./modules/books/book.routes"));
 
 const PORT = process.env.PORT || 5000;
 
