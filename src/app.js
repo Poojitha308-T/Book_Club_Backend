42080af8b74446479ad7b1681dd6abb6ca8b338b
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-03-01 02:24:58 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(
   cors({
     origin:["http://localhost:5173",
-    "https://fluffy-bubblegum-5fd360.netlify.app/"],
+    "https://fluffy-bubblegum-5fd360.netlify.app"],
     credentials: true,
   })
 );
