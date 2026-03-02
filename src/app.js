93d21853aa0d94c314825b547d1acc52e3e41224
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-03-02 16:17:59 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -6,22 +6,25 @@ const errorMiddleware = require("./middleware/error.middleware");
 const app = express();
 
 app.use(express.json());
+
+// Correct CORS configuration
 app.use(
   cors({
-    origin:"http://localhost:5173",
-    
+    origin: [
+      "http://localhost:5173",                     // Local dev
+      "https://9a328140.book-club-frontend.pages.dev" // Cloudflare Pages frontend
+    ],
     credentials: true,
   })
 );
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Book Club Backend is running ",
+    message: "Book Club Backend is running",
   });
 });
 
 app.use("/api", routes); // Central routing
-
 app.use(errorMiddleware);
 
 module.exports = app;
